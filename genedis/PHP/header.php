fe26fbf3e25897a71d554857da4182063696d788
Robiel <89458745+robielcpnv@users.noreply.github.com>
2024-07-24 19:55:12 +0200

    Add header and footer

---
+++ genedis/PHP/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <title>Test Technique - {Tesfazghi Robiel}</title>
+        <meta charset="utf-8">
+
+        <link rel="stylesheet" href="css/custom.css">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/custom.js"></script>
+    </head>
+    <body>
+        <span style="float: right;"><i>Version de l'API : v1.0.0</i></span>
+        <h1>Installations photovoltaïques</h1>
