e0a2a31aee48080dc8b431729ff70d6a0a49b77f
Robiel <89458745+robielcpnv@users.noreply.github.com>
2024-07-24 22:25:22 +0200

    Add chart

--- genedis/PHP/header.php
+++ genedis/PHP/header.php
@@ -7,7 +7,10 @@
         <link rel="stylesheet" href="css/custom.css">
         <script src="js/jquery.min.js"></script>
         <script src="js/custom.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
     </head>
     <body>
         <span style="float: right;"><i>Version de l'API : v1.0.0</i></span>
-        <h1>Installations photovoltaïques</h1>
+        <h1  style="text-align:center; margin:10px">Installations photovoltaïques</h1>
--- genedis/PHP/index.php
+++ genedis/PHP/index.php
@@ -10,12 +10,12 @@
 
         <div style="width: 49%; display: inline-block; text-align: center; vertical-align: top;">
             <h2>Installations par année</h2>
-            <img src="media/bar.png" style="width: 670px;">
+            <canvas id="barChart" width="300" height="300"></canvas>
         </div>
         <div style="width: 49%; display: inline-block; text-align: center; vertical-align: top;">
             <h2>Répartition des capacités par propriétaire</h2>
-            <img src="media/pie.png" style="width: 350px;">
-        </div>
+            <canvas id="pieChart" width="300" height="300"></canvas>
+            </div>
         <div style="width: 100%; display: inline-block; text-align: center; vertical-align: top;">
             <h2>Liste des installations</h2>
         
@@ -52,3 +52,51 @@
       
 include 'footer.php';
 ?>
+
+
+
+<script>
+        const pieChart = document.getElementById('pieChart');
+        const barChart = document.getElementById('barChart');
+
+        new Chart(pieChart, {
+            type: 'pie',
+            data:  {
+                labels: [
+                    'Red',
+                    'Blue',
+                    'Yellow'
+                ],
+                datasets: [{
+                    label: 'My First Dataset',
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                    ],
+                    hoverOffset: 4
+                }]
+            },
+        });
+        new Chart(barChart, {
+            type: 'bar',
+            data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+            },
+            options: {
+            scales: {
+                y: {
+                beginAtZero: true
+                }
+            }
+            }
+        });
+        </script>
+
+    </script>
